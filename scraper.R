d8affa9ead6be8ec6e0ae91f2944e4bd603d8475
321k <erikjohansson01@gmail.com>
2016-04-04 18:07:28 +0100

    basic fixes

--- scraper.R
+++ scraper.R
@@ -8,7 +8,6 @@ results_per_page <- 20
 base_url <- 'https://beta.companieshouse.gov.uk/search/companies?q='
 search <- 'a'
 
-
 get_this <- paste(base_url, search, sep='')
 total_results <- read_html(get_this) %>% 
   html_nodes(xpath='//div//p') %>% 
@@ -27,7 +26,8 @@ pages_to_get <- total_results/results_per_page
 max_iterations = 100000
 pb <- txtProgressBar(min = 0, max = min(pages_to_get, max_iterations), style = 3)
 
-for(iter in max(1, length(res)):min(pages_to_get, max_iterations)){
+for(iter in max(1, length(results_from_companies_house)):min(pages_to_get, max_iterations)){
+  setTxtProgressBar(pb, iter)
   get_this <- paste(base_url, search, '&page=', iter, sep='')
   html_page <- read_html(get_this)
   company_names <- html_page %>% 
@@ -41,23 +41,6 @@ for(iter in max(1, length(res)):min(pages_to_get, max_iterations)){
   as.character %>% 
   substr(19, 26)
   
-  results_from_companies_house[[i]] <- data.frame(company_ids, company_names)
-  setTxtProgressBar(pb, i)
+  results_from_companies_house[[iter]] <- data.frame(company_ids, company_names)
 }
 close(pb)
-
-
-
-
-x %>% 
-  html_node('article') %>% 
-  html_text()
-
-
-x %>% 
-  html_nodes(xpath='//div//h3') %>% 
-  html_text()
-
-
-
-library(data.table)
